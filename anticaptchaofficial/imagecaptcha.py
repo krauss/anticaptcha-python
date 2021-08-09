2bd27f84a91befa7862a9f993b09fa11d0b70de7
Jose R. K <jose.ricardo.k@gmail.com>
2021-08-09 10:08:20 -0300

    add method that accepts byte as argument

--- anticaptchaofficial/imagecaptcha.py
+++ anticaptchaofficial/imagecaptcha.py
@@ -34,3 +34,34 @@ class imagecaptcha(antiNetworking):
             return 0
         else:
             return task_result["solution"]["text"]
+
+    
+    def solve_bytes_and_return_solution(self, b64_byte_img: bytes, **kwargs):        
+        img_str = b64_byte_img.decode('ascii') 
+        task_data = {
+            "type": "ImageToTextTask",
+            "body": img_str,
+            "phrase": self.phrase,
+            "case": self.case,
+            "numeric": self.numeric,
+            "math": self.math,
+            "minLength": self.minLength,
+            "maxLength": self.maxLength,
+            "comment": self.comment
+        }
+        task_data.update(kwargs)
+        if self.create_task({
+            "clientKey": self.client_key,
+            "task": task_data
+        }) == 1:
+            self.log("created task with id "+str(self.task_id))
+        else:
+            self.log("could not create task")
+            self.log(self.err_string)
+            return 0
+
+        task_result = self.wait_for_result(60)
+        if task_result == 0:
+            return 0
+        else:
+            return task_result["solution"]["text"]
